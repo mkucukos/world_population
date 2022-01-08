b09554c31d0a9beed27388769c60925200b95de4
mkucukos <94495145+mkucukos@users.noreply.github.com>
2022-01-07 20:07:25 -0800

    flake 8 applied to the code!

--- world_population.py
+++ world_population.py
@@ -20,7 +20,7 @@ soup = BeautifulSoup(data, "html.parser")
 # in html table is represented by the tag <table>
 tables = soup.find_all('table')
 
-# we can see how many tables were found by checking 
+# we can see how many tables were found by checking
 # the length of the tables list
 len(tables)
 
@@ -32,7 +32,7 @@ print(table_index)
 print(tables[table_index].prettify())
 
 
-# The <td> tag defines the standard cells in the table 
+# The <td> tag defines the standard cells in the table
 # which are displayed as normal-weight, left-aligned text.
 # The <tr> tag defines the table rows.
 population_data = pd.DataFrame(
@@ -47,7 +47,7 @@ for row in tables[table_index].tbody.find_all("tr"):
         area = col[3].text.strip()
         density = col[4].text.strip()
         population_data = population_data.append(
-            {"Rank": rank, "Country": country, "Population": population, 
-            "Area": area, "Density": density}, ignore_index=True)
+            {"Rank": rank, "Country": country, "Population": population,
+             "Area": area, "Density": density}, ignore_index=True)
 
 population_data
